0efc8cafc7fdcea61e2e00ccdc54c3f2117fca67
Paul Dmitryev <clegger@gmail.com>
2021-08-27 11:08:56 +0200

    Initial Commit

---
+++ ToDoom/ToDoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoomApp.swift
+//  ToDoom
+//
+//  Created by Paul Dmitryev on 27.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
